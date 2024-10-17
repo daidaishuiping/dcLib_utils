2305af52283c798106d5e1225ad6b9b6aa1ae754
daichao <380967521@qq.com>
2024-10-18 00:44:52 +0800

    1.0.2 测试

--- src/main/java/com/lib/utils/ToastUtils.java
+++ src/main/java/com/lib/utils/ToastUtils.java
@@ -17,4 +17,8 @@ public class ToastUtils {
     public static void showMsg(Context context, String msg) {
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
+
+    public static void showMsg(Context context, String msg, String msg2) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
 }
